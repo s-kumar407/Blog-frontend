c32d22a71950e99380a81ca3bd25d742ad828238
s-kumar407 <sachinkr407@gmail.com>
2024-08-26 12:56:11 +0530

    commit

--- vite.config.js
+++ vite.config.js
@@ -5,12 +5,7 @@ import { defineConfig } from "vite"
 // https://vitejs.dev/config/
 export default defineConfig({
  plugins: [react()],
- build: {
-  outDir: 'dist'
-},
-server: {
-  open: true
-},
+ 
  resolve: {
    alias: {
      "@": path.resolve(__dirname, "./src"),
